35e8e649cc47b10d27ab24f11819323a79ae5a99
Pachi-Saurio <94994528+PachiSaurio@users.noreply.github.com>
2022-07-28 18:53:35 -0300

    Update
    COMMIT 28Jul22 Update

--- EjemploPOO/src/ejemplopoo/Auto.java
+++ EjemploPOO/src/ejemplopoo/Auto.java
@@ -1,34 +1,79 @@
 package ejemplopoo;
 
-public class Auto {
-    
-    int id;
-    String patente;
-    int can_puertas;
-    String color; 
-    boolean caja_manual;
+import java.util.logging.Logger;
+
+public class Auto extends Vehiculo{
+
+    private int id;
+    private String patente;
+    private int can_puertas;
+    private String color;
+    private boolean caja_manual;
 
     public Auto() {
     }
 
-    public Auto(int id, String patente, int can_puertas, String color, boolean caja_manual) {
+    public Auto(int id, String patente, int can_puertas, String color, boolean caja_manual, int num_registro, int cant_ruedas, String marca, String modelo) {
+        super(num_registro, cant_ruedas, marca, modelo);
         this.id = id;
         this.patente = patente;
         this.can_puertas = can_puertas;
         this.color = color;
         this.caja_manual = caja_manual;
+    }    
+    
+    public int getId() {
+        return id;
     }
-        
-    public void encender () {
+    public String getPatente() {
+        return patente;
+    }
+    public int getCan_puertas() {
+        return can_puertas;
+    }
+    public String getColor() {
+        return color;
+    }
+    public boolean isCaja_manual() {
+        return caja_manual;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setPatente(String patente) {
+        this.patente = patente;
+    }
+    public void setCan_puertas(int can_puertas) {
+        this.can_puertas = can_puertas;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setCaja_manual(boolean caja_manual) {
+        this.caja_manual = caja_manual;
+    }
+
+    @Override
+    public String toString() {
+        return "Auto{" + "id=" + id + ", patente=" + patente + ", can_puertas=" + can_puertas + ", color=" + color + ", caja_manual=" + caja_manual + '}';
+    }
+    
+    
+
+    public void encender() {
         System.out.println("Encendiendo..");
     }
-    public void apagar () {
+
+    public void apagar() {
         System.out.println("Apagando..");
     }
-    public void acelerar (double km) {
+
+    public void acelerar(double km) {
         System.out.println("Acelerando a" + km + "km/h");
-    } 
-    public void frenar (double km) {
-        System.out.println("Frenando a" + km + "km/h");             
+    }
+
+    public void frenar(double km) {
+        System.out.println("Frenando a" + km + "km/h");
     }
 }
--- EjemploPOO/src/ejemplopoo/ejemploPOO.java
+++ EjemploPOO/src/ejemplopoo/ejemploPOO.java
@@ -1,20 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ejemplopoo;
 
-/**
- *
- * @author PACHISAURIO
- */
 public class ejemploPOO {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+    Vehiculo vehi = new Vehiculo(5636, 4, "Renault", "RAf");
+        Auto auto1 = new Auto();
+        Auto auto2 = new Auto(1, "AA22ee", 4, "blue", true, 23423, 4, "VW", "Gol");
+        Moto moto1 = new Moto(125, 1231, 2, "Zanella", "Twe");
+        Camion cami1 = new Camion(6, true, 234234, 6, "Scania", "RAs");
+        
+        Vehiculo vector[] = new Vehiculo [4];
+        vector[0]= vehi;
+        vector[1]= auto2;
+        vector[2]= moto1;
+        vector[3]= cami1;
+        
+            for (int i=0; i<4; i++){
+                System.out.println("Registro Nº" + i + vector[i].getMarca());
+            }
+        /*
+        auto2.acelerar(23.4);
+        auto2.apagar();
+        auto2.acelerar(25.3);
+        auto2.encender();
+    
+        System.out.println("El valor id:" + auto2.getId());
+        auto2.setId(35);
+        System.out.println("El nuevo valor:" +auto2.getId());
+            
+        System.out.println("la id de auto1:" + auto1.getId());
+        System.out.println("el color de auto1:" + auto1.getColor());
+        
+        auto1.setId(2);
+        auto1.setCan_puertas(3);
+        auto1.setColor("azul");
+        auto1.setCaja_manual(false);
+        auto1.setPatente("AA2234");
+        
+        System.out.println("---");        
+        
+        System.out.println("la id de auto1:" + auto1.getId());
+        System.out.println("el color de auto1:" + auto1.getColor());
+        */
+        
     }
     
 }
